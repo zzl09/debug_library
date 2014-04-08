5b28a1b2c7a0b3ceedbc5b939e9a365e3cfeba21
zzl09 <zhang@zhangs-Mac-mini.local>
2014-04-08 17:14:40 +0800

    add Log To File

--- src/org/stackx/debug/ZLog.java
+++ src/org/stackx/debug/ZLog.java
@@ -1,15 +1,34 @@
+/*
+ * Copyright (C) 2014 zzl09
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.stackx.debug;
 
+import android.os.Environment;
+
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ZLog {
     private static final String ANDROID_DEBUG_CLASS_NAME = "android.util.Log";
+    private static final String LogFile = "Zlog.log";
 
     private static StackTraceElement getCurrentStackTrace() {
         StackTraceElement result = null;
-        StackTraceElement[] stackTraceArray = Thread.currentThread()
-                .getStackTrace();
+        StackTraceElement[] stackTraceArray = Thread.currentThread().getStackTrace();
 
         if (stackTraceArray == null) {
             return null;
@@ -37,9 +56,7 @@ public class ZLog {
         String result = "null";
         StackTraceElement currentStackTrace = getCurrentStackTrace();
         if (currentStackTrace != null) {
-            result = currentStackTrace.getFileName() + "-"
-                    + currentStackTrace.getMethodName() + "  (line:"
-                    + currentStackTrace.getLineNumber() + ")";
+            result = currentStackTrace.getFileName() + "-" + currentStackTrace.getMethodName() + "  (line:" + currentStackTrace.getLineNumber() + ")";
         }
         return result;
     }
@@ -53,28 +70,18 @@ public class ZLog {
             for (T tmpMsg : msg) {
                 if (tmpMsg instanceof Iterable) {
                     for (Object singleTmpMsg : (Iterable) tmpMsg) {
-                        logMethod.invoke(null, className,
-                                String.valueOf(singleTmpMsg));
+                        logMethod.invoke(null, className, String.valueOf(singleTmpMsg));
                     }
                 } else if (tmpMsg != null && tmpMsg.getClass().isArray()) {
                     Object[] tmpMsgArray = (Object[]) tmpMsg;
                     for (int i = 0; i < tmpMsgArray.length; i++) {
-                        logMethod.invoke(null, className,
-                                String.valueOf(tmpMsgArray[i]));
+                        logMethod.invoke(null, className, String.valueOf(tmpMsgArray[i]));
                     }
                 } else {
                     logMethod.invoke(null, className, String.valueOf(tmpMsg));
                 }
             }
-        } catch (ClassNotFoundException e) {
-            android.util.Log.e("Debug Model Error", e.toString());
-        } catch (NoSuchMethodException e) {
-            android.util.Log.e("Debug Model Error", e.toString());
-        } catch (IllegalAccessException e) {
-            android.util.Log.e("Debug Model Error", e.toString());
-        } catch (IllegalArgumentException e) {
-            android.util.Log.e("Debug Model Error", e.toString());
-        } catch (InvocationTargetException e) {
+        } catch (Exception e) {
             android.util.Log.e("Debug Model Error", e.toString());
         }
 
@@ -86,7 +93,79 @@ public class ZLog {
 
     public static <T> void v(T... msg) {
         doDebug("v", msg);
+    }
 
+    public static <T> void f(T... msg) {
+        try {
+            String className = getClassMethodName();
+            Class<?> logClass = Class.forName(ANDROID_DEBUG_CLASS_NAME);
+            Class[] parameterTypes = new Class[]{String.class, String.class};
+            for (T tmpMsg : msg) {
+                if (tmpMsg instanceof Iterable) {
+                    for (Object singleTmpMsg : (Iterable) tmpMsg) {
+                        saveToSDCard(LogFile, String.valueOf(singleTmpMsg));
+                        //logMethod.invoke(null, className, String.valueOf(singleTmpMsg));
+                    }
+                } else if (tmpMsg != null && tmpMsg.getClass().isArray()) {
+                    Object[] tmpMsgArray = (Object[]) tmpMsg;
+                    for (int i = 0; i < tmpMsgArray.length; i++) {
+                        saveToSDCard(LogFile, String.valueOf(tmpMsgArray[i]));
+                        //logMethod.invoke(null, className, String.valueOf(tmpMsgArray[i]));
+                    }
+                } else {
+                    saveToSDCard(LogFile, String.valueOf(tmpMsg));
+                    //logMethod.invoke(null, className, String.valueOf(tmpMsg));
+                }
+            }
+        } catch (Exception e) {
+            android.util.Log.e("Debug Model Error", e.toString());
+        }
+    }
+
+    private static void saveToSDCard(String filename, String content) throws Exception {
+        File file = new File(Environment.getExternalStorageDirectory(), filename);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        String handledContent = content + "\r\n";
+        String temp = "";
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        FileOutputStream fos = null;
+        PrintWriter pw = null;
+
+        fis = new FileInputStream(file);
+        isr = new InputStreamReader(fis);
+        br = new BufferedReader(isr);
+        StringBuffer buf = new StringBuffer();
+        for (int j = 1; (temp = br.readLine()) != null; j++) {
+            buf = buf.append(temp);
+            buf = buf.append(System.getProperty("line.separator"));
+        }
+        buf.append(handledContent);
+        fos = new FileOutputStream(file);
+        pw = new PrintWriter(fos);
+        pw.write(buf.toString().toCharArray());
+        pw.flush();
+        if (pw != null) {
+            pw.close();
+        }
+        if (fos != null) {
+            fos.close();
+        }
+        if (br != null) {
+            br.close();
+        }
+        if (isr != null) {
+            isr.close();
+        }
+        if (fis != null) {
+            fis.close();
+        }
+        //OutputStream out=new FileOutputStream(file);
+        //out.write(content.getBytes());
+        //out.close();
     }
 
     public static <T> void d(T... msg) {
